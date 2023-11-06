2e6ce51a0e66680c7f41c4a16834957fee99a9be
Farah Besbes <fbesbes@dom.tti>
2023-11-06 20:55:55 +0100

    act

--- src/test/java/step/Runner2.java
+++ src/test/java/step/Runner2.java
@@ -6,7 +6,9 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features", glue= {"LogSteps"},monochrome=true,
-tags ="@smoke")
+tags ="@tag", plugin = {
+		"pretty", "html:target/rapport.html", "json:target/rapport.json"
+})
 public class Runner2 {
 
 }
